9f685d9008cb217ae43052f9f2ee5ddf0810f6ea
Eben <me@ebenroux.co.za>
2015-11-21 11:51:27 +0200

    - v3.8.0

--- Shuttle.ESB.RabbitMQ.Tests/RabbitMQSection/RabbitMQSectionFixture.cs
+++ Shuttle.ESB.RabbitMQ.Tests/RabbitMQSection/RabbitMQSectionFixture.cs
@@ -1,10 +1,13 @@
+using System;
+using System.IO;
+
 namespace Shuttle.ESB.RabbitMQ.Tests
 {
-	public class RabbitMQSectionFixture
-	{
-		protected RabbitMQSection GetRabbitMQSection(string file)
-		{
-			return RabbitMQSection.Open(string.Format(@".\RabbitMQSection\files\{0}", file));
-		}
-	}
+    public class RabbitMQSectionFixture
+    {
+        protected RabbitMQSection GetRabbitMQSection(string file)
+        {
+            return RabbitMQSection.Open(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, string.Format(@".\RabbitMQSection\files\{0}", file)));
+        }
+    }
 }
