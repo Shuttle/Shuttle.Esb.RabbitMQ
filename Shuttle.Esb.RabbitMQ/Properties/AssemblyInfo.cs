8adfed50d522b7995d26639bdb08b85f5587cb43
Eben <me@ebenroux.co.za>
2016-10-04 18:04:54 +0200

    - v7.0.0

--- Shuttle.Esb.RabbitMQ/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.RabbitMQ/Properties/AssemblyInfo.cs
@@ -1,15 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if NET40FULL
-[assembly: AssemblyTitle("Shuttle.Esb.RabbitMQ for .NET Framework 4.0")]
-#endif
-
-#if NET45FULL
-
-[assembly: AssemblyTitle("Shuttle.Esb.RabbitMQ for .NET Framework 4.5")]
-#endif
-
 #if NET451FULL
 [assembly: AssemblyTitle("Shuttle.Esb.RabbitMQ for .NET Framework 4.5.1")]
 #endif
@@ -26,10 +17,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Esb.RabbitMQ for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("6.0.5.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("6.0.5")]
+[assembly: AssemblyInformationalVersion("7.0.0")]
 [assembly: ComVisible(false)]
