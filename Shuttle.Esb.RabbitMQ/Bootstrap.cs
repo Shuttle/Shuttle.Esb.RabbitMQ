61cc7a0392a13fa09ad917a1be000d5e27310720
Eben <me@ebenroux.co.za>
2017-03-11 14:00:00 +0200

    - package update / bootstrap

---
+++ Shuttle.Esb.RabbitMQ/Bootstrap.cs
@@ -0,0 +1,17 @@
+﻿using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Esb.RabbitMQ
+{
+	public class Bootstrap : IComponentRegistryBootstrap
+	{
+		public void Register(IComponentRegistry registry)
+		{
+			Guard.AgainstNull(registry, "registry");
+
+			if (!registry.IsRegistered<IRabbitMQConfiguration>())
+			{
+				registry.Register<IRabbitMQConfiguration, RabbitMQConfiguration>();
+			}
+		}
+	}
+}
