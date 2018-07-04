cf6ea7215eef7122888f1824cd91b6a5be1201a0
eben-roux <me@ebenroux.co.za>
2018-07-04 21:05:25 +0200

    - v10.0.6

--- Shuttle.Esb.RabbitMQ/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.RabbitMQ/Properties/AssemblyInfo.cs
@@ -1,10 +1,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-#if NET46
-[assembly: AssemblyTitle(".NET Framework 4.6")]
-#endif
-
 #if NET461
 [assembly: AssemblyTitle(".NET Framework 4.6.1")]
 #endif
@@ -25,14 +21,18 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Core 2.0")]
 #endif
 
+#if NETCOREAPP2_1
+[assembly: AssemblyTitle(".NET Core 2.1")]
+#endif
+
 #if NETSTANDARD2_0
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.5.0")]
+[assembly: AssemblyVersion("10.0.6.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
 [assembly: AssemblyProduct("Shuttle.Esb.RabbitMQ")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.5")]
+[assembly: AssemblyInformationalVersion("10.0.6")]
 [assembly: ComVisible(false)]
