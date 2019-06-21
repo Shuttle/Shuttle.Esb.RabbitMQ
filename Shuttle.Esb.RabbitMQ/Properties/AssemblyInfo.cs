177a9319fb1a4bb662c41ca84ded45f0f43acd7d
Eben <me@ebenroux.co.za>
2019-06-21 17:12:08 +0200

    - v11.0.0

--- Shuttle.Esb.RabbitMQ/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.RabbitMQ/Properties/AssemblyInfo.cs
@@ -29,10 +29,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.15.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
+[assembly: AssemblyVersion("11.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2019")]
 [assembly: AssemblyProduct("Shuttle.Esb.RabbitMQ")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.15")]
+[assembly: AssemblyInformationalVersion("11.0.0")]
 [assembly: ComVisible(false)]
